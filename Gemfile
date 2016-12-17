d6f90e057ce955aea1cbfc8bf0ee4aaf5f967368
andrewonyango <andrewonyango@users.noreply.github.com>
2016-12-17 18:39:41 +0300

    validation for user model

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.21'
-gem 'bcrypt-ruby'
+gem 'bcrypt-ruby', '~> 3.0.0'
 gem 'sqlite3'
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
-  attr_accessible :password_digest, :username
+  # remove :password_digest field as we don't want it manipulable by users
+  # add :password and :password_confirmation virtual attributes provided by
+  # has_secure_password
+  attr_accessible :username, :password, :password_confirmation
+
+  has_secure_password
+
+  validates :username,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { in: 4..12 },
+    format: { with: /^[a-z][a-z0-9]*$/,
+              message: "can only use lowercase letters and numbers"}
+
+  validates :password, length: { in: 8..16 }
+  validates :password_confirmation, length: { in: 8..16 }
 end
